55b042bed8289bf86f45a411703ceac862664132
Eric De Sedas <eric.desedas.w@gmail.com>
2015-09-20 18:49:59 -0500

    Initial commit to the repository

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace ExpoHub\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ExpoHub\Providers;
+
+use Illuminate\Contracts\Auth\Access\Gate as GateContract;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'ExpoHub\Model' => 'ExpoHub\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any application authentication / authorization services.
+     *
+     * @param  \Illuminate\Contracts\Auth\Access\Gate  $gate
+     * @return void
+     */
+    public function boot(GateContract $gate)
+    {
+        parent::registerPolicies($gate);
+
+        //
+    }
+}
